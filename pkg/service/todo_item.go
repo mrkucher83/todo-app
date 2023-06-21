d30dfe8048e212e8613c12276260eda167224224
alexanderkucher <mr.kucher83@mail.ru>
2023-06-21 11:00:48 +0400

    * implemented logic of creating todoItem and getting list of items

---
+++ pkg/service/todo_item.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/mrkucher83/todo-app"
+	"github.com/mrkucher83/todo-app/pkg/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo, listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		// list doesn't exists or doesn't belongs to user
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	return s.repo.GetAll(userId, listId)
+}
