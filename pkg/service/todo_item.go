3ed47b3dd19360914ee9f3cc6b1da42e6dd935e9
alexanderkucher <mr.kucher83@mail.ru>
2023-06-21 12:11:08 +0400

    * implemented logic of getting by id, updating and deleting todoItem

--- pkg/service/todo_item.go
+++ pkg/service/todo_item.go
@@ -27,3 +27,18 @@ func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, e
 func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
 	return s.repo.GetAll(userId, listId)
 }
+
+func (s *TodoItemService) GetById(userId, itemId int) (todo.TodoItem, error) {
+	return s.repo.GetById(userId, itemId)
+}
+
+func (s *TodoItemService) Update(userId, itemId int, input todo.UpdateItemInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+	return s.repo.Update(userId, itemId, input)
+}
+
+func (s *TodoItemService) Delete(userId, itemId int) error {
+	return s.repo.Delete(userId, itemId)
+}
